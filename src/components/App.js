727d3af47e13fef45379304d0204e3de7175db28
Suresh-uta <sxk0703@mavs.uta.edu>
2024-02-12 08:22:03 -0600

    wrote code for all the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
-// import AddContact from "./AddContact";
-// import ContactList from "./ContactsList";
+import AddContact from "./AddContact";
+import ContactList from "./ContactsList";
+import ContactCard from "./ContactCard";
 
 
 function App() {
+
+  const contacts =[
+    {
+      id :"1",
+      name :"Mahesh",
+      email :"mahesh@gmail.com"
+    },
+    {
+      id :"2",
+      name :"Suresh",
+      email :"suresh@gmail.com"
+    },
+  ];
   return (
-    <div>
+    <div className="ui container" >
       <Header/>
-      {/* <AddContact/>
-      <ContactList/> */}
+      <AddContact/>
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
